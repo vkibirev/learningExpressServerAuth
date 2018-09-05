96c74cd09ea4f5a497aa2b389f4b8665b9a46f80
kibirev (Виктор Кибирев) <kibirev@office.local>
2018-09-05 17:00:31 +0300

    create user and login user features work

--- script.js
+++ script.js
@@ -5,49 +5,45 @@ const twig = require('twig');
 twig.cache(false);
 const cs = require('cookie-session');
 const connection = mongo.connect('mongodb://127.0.0.1:27017', {useNewUrlParser: true});
+const ObjectId = mongo.ObjectId;
 
 app.use(cs({keys:['test', 'values']}));
 app.use('/public/', express.static('public'));
 app.use(express.urlencoded());
 app.set('view engine', 'twig');
 
-class DB {
-    constructor(dbName, colName, client) {
-        this.dbName = dbName;
-        this.colName = colName;
-        this.client = client;
-    }
-    connectToDb() {
-        return this.client.db(this.dbName);
-    }
-    getCollection() {
-        return this.connectToDb().collection(this.colName);
-    }
-}
+// class DB {
+//     constructor(dbName, colName, client) {
+//         this.dbName = dbName;
+//         this.colName = colName;
+//         this.client = client;
+//     }
+//     connectToDb() {
+//         return this.client.db(this.dbName);
+//     }
+//     getCollection() {
+//         return this.connectToDb().collection(this.colName);
+//     }
+// }
 
 app.get('/userpage', function(req, resp) {    
     connection.then(onConnect
              ).then(onFound
              ).then(checkSessionId);
-
-    console.log('Session active user id', req.session.currentActiveUser); //debug
     
     function onConnect(client) {
         const db = client.db('users');
         const col = db.collection('userData');
-        return col.find({_id:{$eq:req.session.currentActiveUser}});
+        return col.find({"_id":ObjectId(req.session.currentActiveUser)});
     }
 
     function onFound(cursor) {        
         return cursor.toArray();
     }
 
-    function checkSessionId(data) {
-        console.log('Array with found results ', data); //debug
-        
+    function checkSessionId(data) {        
         if(data.length) {
-            console.log('into ', req.session.currentActiveUser);
-            resp.render('userpage');
+            resp.render('userpage', {username: data[0].username});
         } else {
             console.log('wrong id');      
             resp.redirect('/');
@@ -55,42 +51,94 @@ app.get('/userpage', function(req, resp) {
     }
 }); 
 
+app.post('/userpage', function(req, resp) {    
+    if(req.body.logout) {
+        console.log('logged out');        
+        req.session = null;
+        resp.redirect('/');        
+    }
+});
+
 app.get('/', function(req, resp){
     resp.render('login');    
 });
 
 app.post('/', function(req, resp) {
-    connection.then(writeUserDataToDB);
+    connection.then(onConnect
+             ).then(onFound
+             ).then(checkUser
+             ).catch(err);
 
-    function writeUserDataToDB(connection) {
-        let data = {username: req.body.username, password: req.body.password};
-        const db = new DB('users', 'userData', connection);        
-        const userDataCollection = db.getCollection();
-        
-        userDataCollection.find({
-            $and:[
-                {username:{$eq:req.body.username}}, 
-                {password:{$eq:req.body.password}}
+    function onConnect(client) {
+        const db = client.db('users');
+        const col = db.collection('userData');
+
+        return col.find({$and:[
+
+            {"username": req.body.username}, 
+            {"password": req.body.password}
+
             ]
-                        }).toArray(            
-                        ).then(checkUser
-                        ).catch(err);
+        });
+    }
 
-        function checkUser(cursor) { 
-            if (cursor.length) {
-                req.session.currentActiveUser = cursor[0]._id;                      
-                resp.redirect('/userpage');                
-            } else {
-                userDataCollection.insertOne(data);
-                resp.redirect('/userpage');
-            }                        
-        }
+    function onFound(cursor) {        
+        return cursor.toArray();
+    } 
+
+    function checkUser(searchResult) {
+        console.log(searchResult);
+        
+        if(searchResult.length) {
+            req.session.currentActiveUser = searchResult[0]._id;
+            resp.redirect('/userpage');
+        } else {
+            resp.redirect('/');
+            console.log('user need to be created');            
+        }      
+    }    
+
+    function err(err){
+        console.log(err);                       
+    }     
+});
+
+app.get('/createuser', function(req, resp) {
+    resp.render('createuser');
+});
+
+app.post('/createuser', function(req, resp) {
+    let data = {
+        username: req.body.newUserName, 
+        email: req.body.emailField, 
+        password: req.body.newPassword
+    };
+
+    if (req.body.newPassword === req.body.newPasswordConfirm) {
+        connection.then(onConnect
+                 ).then(createUser
+                 ).catch(err);
+
+        function onConnect(client) {
+            const db = client.db('users');
+            const col = db.collection('userData');
+            return col;
+        };
+
+        function createUser(col) {            
+            col.insertOne(data);
+            console.log("New user had been created");
+            resp.redirect('/');
+        };
 
         function err(err){
-            console.log(err);            
-        }        
-    };
-    
+            console.log(err);                       
+        };
+    } else {
+        console.log('Different passwords!');    
+        resp.redirect('/createuser');
+    }
+     
 });
 
 app.listen(3000);
